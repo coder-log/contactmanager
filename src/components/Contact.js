d737585b8cc8bfafe096ccdc28d75fa7b8199832
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-03 06:32:49 +0100

    Refactoring with destructuring the props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export default class Contact extends Component {
   render() {
-      const {name, email, phone} = this.props;
+      const {name, email, phone} = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>{name}</h4>
@@ -18,8 +18,6 @@ export default class Contact extends Component {
 }
 
 Contact.propTypes = {
-  // name: PropTypes.string.isRequired,
-  // email: PropTypes.string.isRequired,
-  // phone: PropTypes.string.isRequired,
+  contact: PropTypes.object.isRequired
 }
 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Contact from './Contact';
 
 class Contacts extends Component {
-    constructor(){
-        super();
-        this.state = {
-            contacts: [
-                {
-                    id:1,
-                    name: 'John Doe',
-                    email: 'jdoe@gmail.com',
-                    phone: '555-555-5555'
-                },
-                
-                {
-                    id:2,
-                    name: 'Karen Williams',
-                    email: 'karen@gmail.com',
-                    phone: '555-555-1111'
-                },
+    state = {
+                contacts: [
+                    {
+                        id:1,
+                        name: 'John Doe',
+                        email: 'jdoe@gmail.com',
+                        phone: '555-555-5555'
+                    },
+                    
+                    {
+                        id:2,
+                        name: 'Karen Williams',
+                        email: 'karen@gmail.com',
+                        phone: '555-555-1111'
+                    },
 
-                {
-                    id:3,
-                    name: 'Henry Johnson',
-                    email: 'henry@gmail.com',
-                    phone: '222-555-1111'
-                }
-            ]
-        }
-    }
+                    {
+                        id:3,
+                        name: 'Henry Johnson',
+                        email: 'henry@gmail.com',
+                        phone: '222-555-1111'
+                    }
+                ]
+            }
   render() {
       const { contacts } = this.state;
 
     return (
       <div>
        {contacts.map(contact=>(
-           <h1>{contact.name}</h1>
+           <Contact 
+                key = {contact.id}
+                contact={contact}  
+            />
        ))}
       </div>
     )
