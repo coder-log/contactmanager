65f8bfd01d1164ae853c376a2d384d599d40e012
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-13 08:19:34 +0100

    Adding a method binded to an event 'onClick' in the icon near the contact name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 
 
 export default class Contact extends Component {
+
+  state = {};
+
+  onShowClick = (e) =>{
+    console.log(console.log(e));
+  }
+
   render() {
       const {name, email, phone} = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>{name}{'  '} 
+        <i onClick = {this.onShowClick.bind(this)} 
+        className="fas fa-sort-down"/>
+        </h4>
         <ul className="list-group"> 
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
