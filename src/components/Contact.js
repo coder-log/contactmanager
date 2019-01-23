03f01d9e0441dfa2fe3fcaf69ab61b900246ebb3
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-23 06:10:08 +0100

    Fixing bug in toggle/untogle contact data.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,21 +9,25 @@ export default class Contact extends Component {
   };
 
   onShowClick = (e) =>{
-    this.setState({ showContactInfo: false });
+    this.setState({ showContactInfo: !this.state.showContactInfo });
   }
 
   render() {
       const { name, email, phone } = this.props.contact;
+      const showContactInfo = this.state.showContactInfo;
     return (
       <div className="card card-body mb-3">
         <h4>{ name }{'  '} 
         <i onClick = {this.onShowClick.bind(this)} 
-        className="fas fa-sort-down"/>
+        className="fas fa-sort-down" style={{cursor: 'pointer'}}/>
         </h4>
-        <ul className="list-group"> 
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? ( 
+          <ul className="list-group"> 
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ): null}
+        
       </div>
     )
   }
